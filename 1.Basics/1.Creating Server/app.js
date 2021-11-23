63a763c122dac930fca6171114cb5fa7de5b60b8
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2021-11-23 20:54:02 +0530

    request and response

--- 1.Basics/1.Creating Server/app.js
+++ 1.Basics/1.Creating Server/app.js
@@ -4,7 +4,17 @@ const http=require('http');
 // Use createServer() method in http and store it in a variable
 
 const server=http.createServer((req,res)=>{
-console.log(req);
+// Accessing request properties
+console.log(req.url,req.method,req.headers);
+// Sending response
+res.setHeader('Content-type','text/html');
+res.write("<html>");
+res.write("<head> <title>My First Response</title></head>");
+res.write("<body><h1>Hello from node server</h1></body>");
+res.end();
+
+// If we want to quite event loop on request we use the following code process.exit()
+// process.exit();
 });
 
 // We have to make our server to listen to request on specific port
