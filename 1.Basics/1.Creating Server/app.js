963055cef1667d88b1bc9cb1c9c93eb8c4ee75e7
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2021-11-24 17:27:49 +0530

    Event drive code execution req.on() and blocking and non blocking code

--- 1.Basics/1.Creating Server/app.js
+++ 1.Basics/1.Creating Server/app.js
@@ -1,18 +1,44 @@
 // To create server we require http core package 
 const http=require('http');
 
+// To handle file system
+const fs=require("fs");
+
 // Use createServer() method in http and store it in a variable
 
 const server=http.createServer((req,res)=>{
 // Accessing request properties
-console.log(req.url,req.method,req.headers);
+// console.log(req.url,req.method,req.headers);
 
 // Routing the requests
 let url=req.url;
 if(url==="/"){
 res.write("<html>");
-res.write("<body><form action='/message' method='post'><input type='text'/> <input type='submit' value='submit' /> </form></body>");
+res.write("<body><form action='/message' method='post'><input name='message' type='text'/> <input type='submit' value='submit' /> </form></body>");
 return res.end();
+}
+
+
+// Redirecting using setHeader Location
+let method=req.method;
+if(url==="/message" && method==="POST")
+{
+// Parsing the incoming request data streams using buffer
+const data=[];
+req.on('data',(chunk)=>{
+console.log(chunk);
+data.push(chunk);
+});
+
+return req.on('end',()=>{
+const parsedBody=Buffer.concat(data).toString();
+const message=parsedBody.split('=')[1];
+fs.writeFileSync("Message.txt",message,()=>{
+    res.statusCode=302;
+    res.setHeader("Location","/");
+    return res.end();
+});
+});
 }
 
 
